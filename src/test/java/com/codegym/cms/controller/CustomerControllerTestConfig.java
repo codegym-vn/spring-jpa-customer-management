b68461bcab2d2777c18f82c3c578d1c28da532c4
Nguyen Khac Nhat <nhat@agilead.vn>
2018-05-27 22:58:22 +0700

    Fix test configuration

--- src/test/java/com/codegym/cms/controller/CustomerControllerTestConfig.java
+++ src/test/java/com/codegym/cms/controller/CustomerControllerTestConfig.java
@@ -8,9 +8,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.codegym.cms")
@@ -20,5 +25,14 @@ public class CustomerControllerTestConfig {
     @Bean
     public CustomerService customerService(){
         return Mockito.mock(CustomerServiecImpl.class);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2)
+                .setName("cms")
+                .build();
+        return db;
     }
 }
--- src/test/java/com/codegym/cms/controller/ProvinceControllerTestConfig.java
+++ src/test/java/com/codegym/cms/controller/ProvinceControllerTestConfig.java
@@ -8,6 +8,11 @@ import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.codegym.cms")
@@ -21,5 +26,14 @@ public class ProvinceControllerTestConfig {
     @Bean
     public CustomerService customerService(){
         return Mockito.mock(CustomerServiecImpl.class);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2)
+                .setName("cms")
+                .build();
+        return db;
     }
 }

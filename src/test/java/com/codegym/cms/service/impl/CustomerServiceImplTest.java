e19e1b8496c717dbf1d5141c6f5168d9603550cb
binhsonnguyen <son@binhsonnguyen.com>
2018-05-28 14:27:15 +0700

    test that service use findAll method of repository

--- src/test/java/com/codegym/cms/service/impl/CustomerServiceImplTest.java
+++ src/test/java/com/codegym/cms/service/impl/CustomerServiceImplTest.java
@@ -1,24 +1,45 @@
 package com.codegym.cms.service.impl;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.codegym.cms.repository.CustomerRepository;
 import com.codegym.cms.service.CustomerService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringJUnitJupiterConfig;
 
 @SpringJUnitJupiterConfig(CustomerServiceImplTestConfig.class)
 public class CustomerServiceImplTest {
 
+  static private Pageable pageable = new PageRequest(0, 20);
+
   private CustomerService customerService;
+  private CustomerRepository customerRepository;
+
+  @AfterEach
+  public void resetAllMockedObject() {
+    Mockito.reset(customerRepository);
+  }
 
   @Test
   public void findAllWithOneCustomer() {
-    assertNotNull(customerService);
+    when(customerRepository.findAll(pageable)).thenReturn(null);
+    customerService.findAll(pageable);
+    verify(customerRepository).findAll(pageable);
   }
 
   @Autowired
   public void setCustomerService(CustomerService customerService) {
     this.customerService = customerService;
+  }
+
+  @Autowired
+  public void setCustomerRepository(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
   }
 }
